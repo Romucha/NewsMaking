e27ee7ef320825ed63a3c643e0835864df5d4c5a
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-06-06 02:36:43 +0300

    Added style dictionary and style for main window.

--- NewsMaking.DesktopApp/Views/MainWindow.xaml.cs
+++ NewsMaking.DesktopApp/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NewsMaking.DesktopApp
+namespace NewsMaking.DesktopApp.Views
 {
 				/// <summary>
 				/// Interaction logic for MainWindow.xaml
