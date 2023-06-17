45a24c6f0ddfc8aaf3a517538dac9a2082c682b2
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-06-17 23:50:58 +0300

    Added custom window template.

---
+++ NewsMaking.DesktopApp/Styles/Styles.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NewsMaking.DesktopApp.Styles
+{
+				public partial class Styles
+				{
+								private void minimize_btn_Click(object sender, System.Windows.RoutedEventArgs e)
+								{
+												Button btn = sender as Button;
+												(btn.TemplatedParent as Window).WindowState = WindowState.Minimized;
+								}
+
+								private void close_btn_Click(object sender, System.Windows.RoutedEventArgs e)
+								{
+												Button btn = sender as Button;
+												(btn.TemplatedParent as Window).Close();
+								}
+				}
+}
