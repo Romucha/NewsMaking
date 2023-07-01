31727f46332ace53d38b5b7245d98dc8822d3edb
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-07-01 21:52:22 +0300

    Added announce part of Ascon news.

---
+++ NewsMaking.AsconNews/Models/AnnounceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsMaking.AsconNews.Models
+{
+ public class AnnounceModel
+ {
+  public string Title { get; set; }
+
+  public string Body { get; set; }
+
+  public Uri Link { get; set; }
+ }
+}
---
+++ NewsMaking.AsconNews/Pages/Announce.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using NewsMaking.AsconNews.Models;
+using NewsMaking.AsconNews.Services;
+
+namespace NewsMaking.AsconNews.Pages
+{
+ public partial class Announce
+ {
+  [Inject]
+  private IAsconService _asconService { get; set; }
+
+  private AnnounceModel _currentAnnounce { get; set; }
+
+  protected override async Task OnInitializedAsync()
+  {
+   _currentAnnounce = await _asconService.GetAnnounce();
+  }
+ }
+}
