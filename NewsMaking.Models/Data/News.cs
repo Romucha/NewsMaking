32d6b0d7156e99aae5a843efd6939c6a6ce5355d
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-07-18 01:44:30 +0300

    Added project for models.

---
+++ NewsMaking.Models/Data/News.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsMaking.Models.Data
+{
+	internal class News
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public string Title { get; set; }
+
+		[Required]
+		public DateTime CreationTime { get; set; }
+
+		[Required]
+		public string ContentType { get; set; }
+
+		[Required]
+		public string Content { get; set; }
+
+		public Uri Link { get; set; }
+
+
+		public NewsUser CreatedBy { get; set; }
+	}
+}
